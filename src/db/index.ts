feb9436a7637e9ef47a9cc9f3ed07589ea2950a0
brunoGermano <brunogermano3@gmail.com>
2025-08-18 11:28:51 -0300

    chore: add better-auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,6 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!); // "process.env.DATABASE_URL!" é a url do banco de dados
+// importing all the configs to create my db, importing like "schema" name. afterall my "db" variable created here will know which schema we use to create our postgresql data base
+import * as schema from "./schema";
+export const db = drizzle(process.env.DATABASE_URL!, { schema }); // "process.env.DATABASE_URL!" é a url do banco de dados
