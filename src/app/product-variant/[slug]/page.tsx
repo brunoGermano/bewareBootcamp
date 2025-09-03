22dbe17b6ea079d5776ce640bf7ad70009938af8
brunoGermano <brunogermano3@gmail.com>
2025-09-03 07:12:25 -0300

    feat: add product page

---
+++ src/app/product-variant/[slug]/page.tsx
@@ -0,0 +1,103 @@
+import { eq } from "drizzle-orm";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { productTable, productVariantTable } from "@/db/schema";
+import { formatCentsToBRL } from "@/helpers/money";
+
+import QuantitySelector from "./components/quantity-selector";
+import VariantSelector from "./components/variant-selector";
+
+interface ProductVariantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
+  const { slug } = await params;
+  const productVariant = await db.query.productVariantTable.findFirst({
+    where: eq(productVariantTable.slug, slug),
+    with: {
+      product: {
+        with: {
+          variants: true,
+        },
+      },
+    },
+  });
+
+  if (!productVariant) {
+    return notFound();
+  }
+
+  // Getting the Similar Products too. Take care don't forget to use "await" before "db" because is an async operation and the error given is not familiar to help you to solve it.
+  const likelyProducts = await db.query.productTable.findMany({
+    where: eq(productTable.categoryId, productVariant.product.categoryId),
+    with: {
+      variants: true,
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col space-y-6">
+        <Image
+          src={productVariant.imageUrl}
+          alt={productVariant.name}
+          sizes="100vw"
+          height={0}
+          width={0}
+          className="h-auto w-full object-cover"
+        />
+
+        <div className="px-5">
+          {
+            /* Variants */
+            <VariantSelector
+              selectedVariantSlug={productVariant.slug}
+              variants={productVariant.product.variants}
+            />
+          }
+        </div>
+
+        <div className="px-5">
+          {/* DESCRIÇÃO */}
+          <h2 className="text-lg font-semibold">
+            {productVariant.product.name}
+          </h2>
+          <h3 className="text-muted-foreground text-sm">
+            {productVariant.name}
+          </h3>
+          <h3 className="text-lg font-semibold">
+            {formatCentsToBRL(productVariant.priceInCents)}
+          </h3>
+        </div>
+
+        <div className="px-5">
+          {/* QUANTIDADE */}
+          <QuantitySelector />
+        </div>
+        <div className="px-5">
+          {/* BUTTONS */}
+          <Button className="rounded-full" size="lg" variant="outline">
+            Adicionar à sacola
+          </Button>
+          <Button className="rounded-full" size="lg">
+            Comprar agora
+          </Button>
+        </div>
+        <div className="px-5">
+          <p className="text-sm">{productVariant.product.description}</p>
+        </div>
+
+        <ProductList title="Talvez você goste" products={likelyProducts} />
+      </div>
+    </>
+  );
+};
+
+export default ProductVariantPage;
